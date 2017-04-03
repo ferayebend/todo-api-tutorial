94d4fc76437c4a5cbbe7853a3c36db93440f80df
bkulebi <bkulebi@bmat.com>
2017-04-03 16:43:31 +0200

    missing test files

---
+++ tests/test_api.py
@@ -0,0 +1,57 @@
+import unittest
+import json
+from flask import url_for
+from api import create_app, db
+
+class APITestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_404(self):
+        response = self.client.get('/wrong/url')
+        self.assertTrue(response.status_code == 404)
+        json_response = json.loads(response.data.decode('utf-8'))
+        self.assertTrue(json_response['error'] == 'not found')
+
+    def test_task(self):
+        test_task = {'title':'Smash Patriarchy'}
+        
+        #create task
+        response = self.client.post(url_for('api.create_task'),
+                         data=json.dumps(test_task),
+                         content_type='application/json')
+        self.assertEquals(response.status_code,201)
+        json_response = json.loads(response.data.decode('utf-8'))
+        self.assertTrue(json_response['title'],test_task['title'])
+        inputted_id = json_response['id']
+
+        #get task
+        response = self.client.get(url_for('api.get_task',
+                                   task_id = inputted_id,
+                                   _external = True))
+        json_response = json.loads(response.data.decode('utf-8'))
+        self.assertTrue(json_response['title'],test_task['title'])
+
+        #update task
+        response = self.client.put(url_for('api.update_task',
+                                           task_id = inputted_id),
+                                   data=json.dumps({'done':True}),
+                                   content_type='application/json')
+        self.assertEquals(response.status_code,201)
+        json_response = json.loads(response.data.decode('utf-8'))
+        self.assertTrue(json_response['done'],True)
+
+        #delete task
+        response = self.client.delete(url_for('api.delete_task',
+                                              task_id = inputted_id))
+        json_response = json.loads(response.data.decode('utf-8'))
+        self.assertEquals(response.status_code,201)
