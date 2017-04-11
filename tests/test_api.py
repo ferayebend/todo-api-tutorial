81854278544f3c20339ff5f9d1408b93955a9cc8
bkulebi <bkulebi@bmat.com>
2017-04-11 17:48:06 +0200

    major changes for auth implementation

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,6 +4,8 @@ from flask import url_for
 from api import create_app, db
 from api.models import User
 
+from test_client import TestClient
+
 class APITestCase(unittest.TestCase):
     default_username = 'lenin'
     default_password = '1917'
@@ -12,8 +14,13 @@ class APITestCase(unittest.TestCase):
         self.app = create_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
-        self.client = self.app.test_client()
         db.create_all()
+        u = User(name=self.default_username,
+                 password=self.default_password)
+        db.session.add(u)
+        db.session.commit()
+
+        self.client = TestClient(self.app, u.generate_auth_token(),'')
 
     def tearDown(self):
         db.session.remove()
@@ -21,77 +28,56 @@ class APITestCase(unittest.TestCase):
         self.app_context.pop()
 
     def test_404(self):
-        response = self.client.get('/wrong/url')
+        response, json_response = self.client.get('/wrong/url')
         self.assertTrue(response.status_code == 404)
-        json_response = json.loads(response.data.decode('utf-8'))
         self.assertTrue(json_response['error'] == 'not found')
 
     def test_task(self):
         test_task = {'title':'Smash Patriarchy'}
         
         #create task
-        response = self.client.post(url_for('api.create_task'),
-                         data=json.dumps(test_task),
-                         content_type='application/json')
+        response, json_response = self.client.post(url_for('api.create_task'),
+                         data=test_task)
         self.assertEquals(response.status_code,201)
-        json_response = json.loads(response.data.decode('utf-8'))
         self.assertTrue(json_response['title'],test_task['title'])
         inputted_id = json_response['id']
 
         #get task
-        response = self.client.get(url_for('api.get_task',
-                                   task_id = inputted_id,
-                                   _external = True))
-        json_response = json.loads(response.data.decode('utf-8'))
+        response, json_response = self.client.get(url_for('api.get_task',
+                                                  task_id = inputted_id,
+                                                   _external = True))
         self.assertTrue(json_response['title'],test_task['title'])
 
         #update task
-        response = self.client.put(url_for('api.update_task',
-                                           task_id = inputted_id),
-                                   data=json.dumps({'done':True}),
-                                   content_type='application/json')
+        response, json_response = self.client.put(url_for('api.update_task',
+                                                  task_id = inputted_id),
+                                                  data={'done':True})
         self.assertEquals(response.status_code,201)
-        json_response = json.loads(response.data.decode('utf-8'))
         self.assertTrue(json_response['done'],True)
 
         #delete task
-        response = self.client.delete(url_for('api.delete_task',
-                                              task_id = inputted_id))
-        json_response = json.loads(response.data.decode('utf-8'))
+        response, json_response = self.client.delete(url_for('api.delete_task',
+                                                     task_id = inputted_id))
         self.assertEquals(response.status_code,201)
 
     def test_user(self):
         test_user = {'name':'Lenin'}
 
         #create user
-        response = self.client.post(url_for('api.create_user'),
-                                   data=json.dumps(test_user),
-                                   content_type='application/json')
+        response, json_response = self.client.post(url_for('api.create_user'),
+                                                   data=test_user)
         self.assertEquals(response.status_code,201)
-        json_response = json.loads(response.data.decode('utf-8'))
         self.assertTrue(json_response['name'],test_user['name'])
         inputted_id = json_response['id']
 
         #get user
-        respose = self.client.get(url_for('api.get_user',
-                                          user_id = inputted_id,
-                                          _external = True))
-        json_response = json.loads(response.data.decode('utf-8'))
+        respose, json_response = self.client.get(url_for('api.get_user',
+                                                 user_id = inputted_id,
+                                                 _external = True))
         self.assertTrue(json_response['name'],test_user['name'])
 
         #delete user
-        response = self.client.delete(url_for('api.delete_user',
-                                              user_id = inputted_id))
-        json_response = json.loads(response.data.decode('utf-8'))
+        response, json_response = self.client.delete(url_for('api.delete_user',
+                                                     user_id = inputted_id))
         self.assertEquals(response.status_code,201)
 
-    def test_user_auth(self):
-        u = User(name=self.default_username,
-                 password=self.default_password)
-        db.session.add(u)
-        db.session.commit()
-
-        #test an api call which needs user verification
-
-        #test token auth
-        
